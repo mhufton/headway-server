2f1d314b2d564cdd70a4b6df37609cae309a736e
Mike Hufton <mikehufton@Mikes-MacBook.local>
2022-03-07 18:36:45 -0800

    messing with testing

---
+++ test/server.test.js
@@ -0,0 +1,53 @@
+const expect = require('chai').expect;
+const url = 'http://localhost:4000/';
+const { resolvers } = '../src/resolvers'
+const { Link } = '../models/link'
+const request = require('supertest')(url);
+const typeDefs = '../src/schema'
+const EasyGraphQLTester = require('easygraphql-tester');
+
+describe('GraphQL', () => {
+  it('Returns a list of all Links', (done) => {
+    request.post('/')
+      .send({ query: 'links: [Link]'})
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.Link.should.have.property('url')
+        res.body.Link.should.have.property('slug')
+        done()
+      })
+  })
+})
+
+describe('Test queries', () => {
+  let tester
+
+  before(() => {
+    tester = new EasyGraphQLTester(typeDefs, resolvers)
+  })
+
+  it('Should pass if the query is valid', () => {
+    const validQuery = `
+    {
+      links {
+        url
+      }
+    }`
+    tester.test(true, validQuery, {
+      links: links
+    })
+  })
+
+  it('Should fail if the query is invalid', () => {
+    const invalidQuery = `
+    {
+      links {
+        longUrl
+      }
+    }
+    `
+    tester.test(false, invalidQuery)
+
+  })
+})
